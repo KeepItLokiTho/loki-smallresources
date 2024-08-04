d191096f499d47e82fc5c815a35e8b5c67a48126
Loki <126781899+KeepItLokiTho@users.noreply.github.com>
2024-08-04 16:43:21 -0500

    initial commit

---
+++ config.lua
@@ -0,0 +1,16 @@
+Config = {}
+
+-- Allow driver to shoot
+Config.DriverDriveBy = false
+
+-- Allow passengers to shoot
+Config.PassengerDriveBy = true
+
+-- Force first person in vehicles
+Config.ForceFirstPersonVeh = true
+
+-- Prevent players from losing hats/glasses when being punched
+Config.PreventPropLoss = true
+
+-- Prevent players from zooming all the way out on foot and in vehicles
+Config.LimitZoomLevel = true
---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+game 'gta5'
+name 'loki-smalltweaks'
+description 'A collection of small quality of life changes'
+version '1.0.0'
+
+shared_script 'config.lua'
+
+client_script 'client/main.lua'
+
+lua54 'yes'
